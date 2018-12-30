40b25732d8121951a9732d9b1d815e8a5445f96c
Andrei Igna <andrei.igna@layered.studio>
2018-12-30 21:33:49 +0100

    Added - small lib to take clean website screenshots

---
+++ README.md
@@ -0,0 +1,63 @@
+# take-screenshots
+
+**take-screenshots** is a small JS library than can take clean website screenshots, without any popups or cookie consent dialogs that may appear when browsing.
+
+It uses [Puppeteer](https://github.com/GoogleChrome/puppeteer) with a set of optimal settings for loading pages & cleaning any unwanted sections (any contributions for this are welcome).
+
+## Use cases
+* Display Info Cards of link hover/tap
+* Rich preview for links in messaging apps or blog articles
+* Automatic site tracking with screenshots
+
+## Getting Started
+
+#### Installation
+
+To use Puppeteer in your project, run:
+
+```npm i take-screenshots```
+
+> Note: `take-screenshots` requires Puppeteer to be installed in your project. It is a peer dependency so that the correct version or build is used on your app.
+
+#### Usage
+The library has a simple API.
+
+**Example** - Take a screenshot of twitter.com and save it as `twitter.png`:
+```
+const takeScreenshot = require('take-screenshots');
+
+(async () => {
+	await takeScreenshot('https://twitter.com', {screenshot: {path: 'twitter.png'}});
+
+	await takeScreenshot.closeBrowser();
+})();
+```
+
+Main function returns the image [buffer](https://nodejs.org/api/buffer.html), which you can manipulate or send forwards as a HTTP Response
+**Example** - Take a screenshot of nyt.com and process the image:
+```
+const takeScreenshot = require('take-screenshots');
+
+(async () => {
+	let img = await takeScreenshot('https://twitter.com', {viewport: {width: 800, height: 600}});
+
+    // process the image, img is the image buffer
+    // sharp(img)
+    
+    // if using Express, image can be sent as response
+    // res.send(img)
+
+	await takeScreenshot.closeBrowser();
+})();
+```
+
+## More
+
+Please report any issues here on GitHub.
+[Any contributions are welcome](CONTRIBUTING.md)
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
+
+Copyright (c) Andrei Igna
---
+++ index.js
@@ -0,0 +1,109 @@
+const puppeteer = require('puppeteer')
+const _ = require('lodash')
+
+let browser
+
+const getBrowserPage = async () => {
+
+	if (!browser) {
+		browser = await puppeteer.launch({args: ['--no-sandbox']})
+	}
+
+	return browser.newPage()
+}
+
+let takeScreenshots = async function(url, options) {
+	options = _.defaults(options, {
+		userAgent:		'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+		animationSpeed:	20,
+		pageDelay:		250,
+		headers:		{
+			'Accept-Language':	'en;q=0.9'
+		},
+		viewport:		{
+			width:			1280,
+			height:			800,
+			isMobile:		false,
+			isLandscape:	false
+		},
+		screenshot:		{
+			type:			'png',
+			fullPage:		false
+		}
+	});
+
+	const page = await getBrowserPage();
+
+	// set User Agent & headers - bypass Captchas or popups about security on some websites
+	await page.setUserAgent(options.userAgent)
+	await page.setExtraHTTPHeaders(options.headers)
+
+	// set viewport options, see https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagesetviewportviewport
+	await page.setViewport(options.viewport)
+
+	await page.goto(url, { waitUntil: 'networkidle2' })
+
+	if (options.animationSpeed) {
+		await page._client.send('Animation.setPlaybackRate', { playbackRate: Number.isInteger(options.animationSpeed) ? options.animationSpeed : 20 })
+	}
+
+	// clean - cookie consent dialogs
+	let dialogClasses = ['[role="dialog"]'];
+	['class', 'id'].forEach(attr => {
+		['consent', 'gdpr', 'cookie', 'banner', 'privacy', 'ads'].forEach(keyword => dialogClasses.push('[' + attr + '*="' + keyword + '"]'))
+	});
+	await page.$$eval(dialogClasses.join(','), elements => {
+		elements.forEach(el => {
+			const elContent = el.innerText.toLowerCase().trim()
+			if (!elContent.length || elContent.includes('cookie') || elContent.includes('consent') || elContent.includes('newsletter') || elContent.includes('subscribe')) {
+				let maybeOverlay = [
+					el.previousSibling,
+					el.nextSibling,
+					el.parentElement,
+					el.parentElement.previousSibling,
+					el.parentElement.nextSibling
+				];
+				el.remove();
+				maybeOverlay.forEach(elOverlay => {
+					if (elOverlay && elOverlay.className && elOverlay.className.includes('overlay')) {
+						elOverlay.remove();
+					}
+				});
+			}
+		});
+	});
+
+	// clean - notifications and dialogs
+	await page.$$eval('[class*="toast"], [class*="alert"], [role="dialog"], [class*="dialog"]', elements => {
+		elements.forEach(el => el.remove())
+	});
+
+	// clean - Quantcast cookie consent dialog
+	try {
+		await page.$eval('.qc-cmp-button', element => {
+			if (element && '__cmpui' in window) {
+				window.__cmpui('setAndSaveAllConsent', true);
+			}
+		});
+	} catch (err) {}
+
+	// delay for good measure. Animation playback rate is multiplied also
+	await page.waitFor(options.pageDelay)
+
+	// set screenshot options, see https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagescreenshotoptions
+	const imageBuffer = await page.screenshot(options.screenshot)
+	await page.close()
+
+	return imageBuffer
+}
+
+takeScreenshots.setBrowser = function(newBrowser) {
+	return browser = newBrowser
+}
+
+takeScreenshots.closeBrowser = async function() {
+	await browser.close()
+	browser = null
+}
+
+module.exports = takeScreenshots
